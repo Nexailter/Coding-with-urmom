45d5546caddd595ef270c592e8ef561b691b734f
Nexailter <100661158+Nexailter@users.noreply.github.com>
2022-03-29 19:43:55 -0700

    Made a lot of more THINGIMIGIGS

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import Thing
+
+# Classes
+classes = ['Demon','Wizard','Hunter']
+
+# Elements
+element = ['earth','fire','Ice','Lightning']
+
 #get the users input, ask what class they want to use
+user_class = input('What class would you like to choose')
+
+# Get the users choice of element
+
+# Get the user's character name
+
+# Check if user_class variable is Demon,Wizard or Hunter
+if user_class == 'Demon':
+    Thing.demon
